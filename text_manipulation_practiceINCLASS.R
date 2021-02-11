34c2b0dacd751b36f281dfb6c8ed3dcfed5daf0c
LiamNMcCall <78005318+LiamNMcCall@users.noreply.github.com>
2021-02-11 14:33:10 -0500

    Add files via upload

---
+++ text_manipulation_practiceINCLASS.R
@@ -0,0 +1,17 @@
+# Manupulating text in R
+
+#1. Find a sentence online. Save it as a string. 
+#"Kintzler gets minor league deal from Phils"
+require(quanteda)
+
+#2. Select only the third word of the sentence. Save it as a new string.
+#"gets"
+
+#3. Choose a letter that appears in your sentence. Use the gsub command to replace all instances of that letter with a period. 
+gsub("e",".",s1)
+
+
+
+
+
+
