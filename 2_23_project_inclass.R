5a09d8a2ee507da2f68aee8ba032d30727f40390
LiamNMcCall <78005318+LiamNMcCall@users.noreply.github.com>
2021-02-23 14:42:34 -0500

    Add files via upload

---
+++ 2_23_project_inclass.R
@@ -0,0 +1,13 @@
+#What research question do you want to answer?
+#How does the lexical similarity between the statements of GOP senators re mass-shootings compare to that of statements re different events?
+
+#What are two possible answers?
+#Either there is a higher degree of lexical similarity between mass shooting statements or the degree is consistent with that of other statements re different events (budget hearings, foreign policy sessions, etc).
+  
+#What are the documents of text data you will analyze?
+#the documents will be individual statements (speeches or pullen from twitter/their press sec.) of GOP senators
+  
+  
+  
+#Give two examples of documents in the corpus that you expect will be high in similarity to each other? Two examples that are *low* in similarity?
+#I expect two documents from the mass shooting corpus to display high levels of similarity due senators following a cut and dry "Playbook" for statements on mass shootings. I expect two documents from a budget hearing Congressional session to have low similarity since there is a schism between war-hawk GOP senators and the so-called deficit hawks.
